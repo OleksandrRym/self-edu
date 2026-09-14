154c099b22c9f1aaa5eae3137f19dc18ee72ff4b
OleksandrRym <lostmannight@gmail.com>
2026-09-14 23:03:10 +0300

    add Barier demo

---
+++ grind/src/book/java_cc/CyclicBarrierDemo.java
@@ -0,0 +1,41 @@
+package book.java_cc;
+
+import java.util.List;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static java.lang.Thread.sleep;
+
+public class CyclicBarrierDemo {
+    private final static CyclicBarrier CYCLIC_BARRIER  =new CyclicBarrier(2,()->{
+        System.out.println("start fight");
+    });
+
+    static ExecutorService pool = Executors.newCachedThreadPool();
+    private static final List<String> names = List.of("Oleg", "Alex");
+
+    static void main() throws InterruptedException {
+       pool.execute(()->{
+           try {
+               System.out.println("Ready " + names.get(0));
+               sleep(3000);
+               CYCLIC_BARRIER.await();
+               System.out.println("start" + names.get(1));
+           } catch (InterruptedException | BrokenBarrierException e) {
+               throw new RuntimeException(e);
+           }
+       });
+        pool.execute(()->{
+            try {
+                System.out.println("Ready " + names.get(1));
+                sleep(3000);
+                CYCLIC_BARRIER.await();
+                System.out.println("start" + names.get(1));
+            } catch (InterruptedException | BrokenBarrierException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+}
